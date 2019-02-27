e6ae0322f92110d125f517cedc4973690d5e8609
Andrew Udvare <audvare@gmail.com>
2019-02-27 00:03:55 -0500

    Add touch ID support under HAVE_TOUCHID

--- sudo/plugins/sudoers/auth/sudo_auth.m
+++ sudo/plugins/sudoers/auth/sudo_auth.m
@@ -34,11 +34,29 @@
 #include <time.h>
 #include <signal.h>
 
+#ifdef HAVE_TOUCHID
+#include <crt_externs.h>
+#endif // __APPLE__
+
 #include "sudoers.h"
 #include "sudo_auth.h"
 #include "insults.h"
 
+#ifdef HAVE_TOUCHID
+#import <LocalAuthentication/LocalAuthentication.h>
+int is_over_ssh = 0;
+int touchid_setup(struct passwd *pw, char **prompt, sudo_auth *auth);
+int touchid_verify(struct passwd *pw, char *pass, sudo_auth *auth, struct sudo_conv_callback *callback);
+int touchid_pam_begin_session(struct passwd *pw, char **user_envp[], sudo_auth *auth);
+int touchid_pam_end_session(struct passwd *pw, sudo_auth *auth);
+#endif // HAVE_TOUCHID
+
 static sudo_auth auth_switch[] = {
+#ifdef HAVE_TOUCHID
+    /* Touch ID, with fallback to PAM when over SSH */
+    AUTH_ENTRY("touchid", FLAG_STANDALONE, sudo_pam_init, touchid_setup, touchid_verify, sudo_pam_cleanup, touchid_pam_begin_session, touchid_pam_end_session)
+#endif
+    
 /* Standalone entries first */
 #ifdef HAVE_AIXAUTH
     AUTH_ENTRY("aixauth", FLAG_STANDALONE, sudo_aix_init, NULL, sudo_aix_verify, sudo_aix_cleanup, NULL, NULL)
@@ -116,6 +134,7 @@ sudo_auth_init(struct passwd *pw)
      * Make sure we haven't mixed standalone and shared auth methods.
      * If there are multiple standalone methods, only use the first one.
      */
+    
     if ((standalone = IS_STANDALONE(&auth_switch[0]))) {
 	bool found = false;
 	for (auth = auth_switch; auth->name; auth++) {
@@ -445,3 +464,84 @@ dump_auth_methods(void)
 
     debug_return;
 }
+
+#ifdef HAVE_TOUCHID
+typedef enum {
+    kTouchIDResultNone,
+    kTouchIDResultAllowed,
+    kTouchIDResultFailed
+} TouchIDResult;
+
+static const LAPolicy kAuthPolicy = 0x3f0;
+
+int
+touchid_setup(struct passwd *pw, char **prompt, sudo_auth *auth) {
+    // If over SSH, indicated by non-empty environment variable SSH_CONNECTION,
+    // fallback to PAM by exiting early as possible here
+    // Still has to be AUTH_SUCCESS but is_over_ssh is set to non-zero
+    char **ep, **envp = *_NSGetEnviron();
+    const char *name = "SSH_CONNECTION";
+    size_t namelen = 0;
+    while (name[namelen] != '\0') {
+        namelen++;
+    }
+    for (ep = envp; *ep != NULL; ep++) {
+        if (strncmp(*ep, name, namelen) == 0 && (*ep)[namelen] == '=') {
+            log_warningx(SLOG_SEND_MAIL, N_("No Touch ID over SSH."));
+            is_over_ssh = 1;
+            return AUTH_SUCCESS;
+        }
+    }
+
+    @try {
+        LAContext *context = [[LAContext alloc] init];
+        BOOL canAuthenticate = [context canEvaluatePolicy:kAuthPolicy error:nil];
+        [context release];
+        if (canAuthenticate) {
+            return AUTH_SUCCESS;
+        }
+    }
+    @catch(NSException *) {
+        // LAPolicyDeviceOwnerAuthenticationWithBiometrics may not be available on builds older than 10.12.1!
+        sudo_printf(SUDO_CONV_INFO_MSG, _("2"));
+    }
+    audit_failure(NewArgc, NewArgv, "%s", N_("Touch ID setup failed."));
+
+    return AUTH_FAILURE;
+}
+
+int
+touchid_verify(struct passwd *pw, char *pass, sudo_auth *auth, struct sudo_conv_callback *callback) {
+    if (is_over_ssh) {
+        return sudo_pam_verify(pw, pass, auth, callback);
+    }
+
+    LAContext *context = [[LAContext alloc] init];
+    __block TouchIDResult result = kTouchIDResultNone;
+    [context evaluatePolicy:kAuthPolicy localizedReason:@"authenticate a privileged operation" reply:^(BOOL success, NSError *error) {
+        result = success ? kTouchIDResultAllowed : kTouchIDResultFailed;
+        CFRunLoopWakeUp(CFRunLoopGetCurrent());
+    }];
+    
+    while (result == kTouchIDResultNone)
+        CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0, true);
+    
+    [context release];
+    return result == kTouchIDResultAllowed ? AUTH_SUCCESS : AUTH_FAILURE;
+}
+
+int touchid_pam_begin_session(struct passwd *pw, char **user_envp[], sudo_auth *auth) {
+    if (is_over_ssh) {
+        return sudo_pam_begin_session(pw, user_envp, auth);
+    }
+    return AUTH_SUCCESS;
+}
+
+int touchid_pam_end_session(struct passwd *pw, sudo_auth *auth) {
+    if (is_over_ssh) {
+        return sudo_pam_end_session(pw, auth);
+    }
+    return AUTH_SUCCESS;
+}
+#endif // HAVE_TOUCHID
+
